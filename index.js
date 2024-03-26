6a178c6eac221a9fad7be7923e0d7c835cfdc0c2
Yordan Vladov <jordan0vladov@gmail.com>
2024-03-26 15:46:35 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path');
+
+const app = express()
+const port = process.env.PORT || 8080;
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
