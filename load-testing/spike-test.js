d37a8fd4a8a9d92b01ec1797259feeee01117899
yokoops <94515191+yokoops@users.noreply.github.com>
2024-03-28 11:35:46 +0200

    Create spike-test.js

---
+++ load-testing/spike-test.js
@@ -0,0 +1,17 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  // Key configurations for spike in this section
+  stages: [
+    { duration: '2m', target: 2000 }, // fast ramp-up to a high point
+    // No plateau
+    { duration: '1m', target: 0 }, // quick ramp-down to 0 users
+  ],
+};
+
+export default () => {
+  const urlRes = http.get('http://localhost:8080');
+  sleep(1);
+
+};
