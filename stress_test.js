9dbfb0c6699047a3573308b6841d7b7e3abf83a0
IvayloDZH05 <116313439+IvayloDZH05@users.noreply.github.com>
2024-04-03 23:58:30 +0300

    Add files via upload

---
+++ stress_test.js
@@ -0,0 +1,15 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '10m', target: 200 }, 
+    { duration: '30m', target: 200 }, 
+    { duration: '5m', target: 0 }, 
+  ],
+};
+
+export default () => {
+  const urlRes = http.get('http://localhost:8080');
+  sleep(1);
+};
