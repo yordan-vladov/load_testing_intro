8a4dbd3ed8758ecf008a65b83f0468f9ef12c727
OMGCAT <19413@uktc-bg.com>
2024-04-04 20:02:52 +0300

    Dennis Fraser 19413

---
+++ load-testing/loadtest.js
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  
+  stages: [
+    { duration: '5m', target: 100 }, 
+    { duration: '30m', target: 100 }, 
+    { duration: '5m', target: 0 }, 
+  ],
+};
+
+export default () => {
+  const urlRes = http.get('https://test-api.k6.io');
+  sleep(1);
+};
