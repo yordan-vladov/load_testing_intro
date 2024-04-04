7b2a10b4fe1533f72372bb25c759a7ece3229879
Zhori 05 <114079886+Zhori05@users.noreply.github.com>
2024-04-04 22:45:00 +0300

    19307 load тестове
    testove

---
+++ load-testing/spikeTest.js
@@ -0,0 +1,14 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+    stages: [
+        { duration: '2m', target: 2000 },
+        { duration: '1m', target: 0 },
+    ],
+};
+
+export default () => {
+    const urlRes = http.get('https://test-api.k6.io');
+    sleep(1);
+};
