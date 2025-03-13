77dac0e71857d0ea287c60e2e62b3dba43f7b36e
yordan-vladov <jordan0vladov@gmail.com>
2025-03-13 08:15:22 +0200

    Changed formatting

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-const express = require('express')
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 const port = process.env.PORT || 8080;
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
-})
-
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
   res.send(req.body);
-})
+});
 
-
-app.post('/capitalize', (req, res) => {
-  ans = {...req.body};
+app.post("/capitalize", (req, res) => {
+  ans = { ...req.body };
   ans.name = ans.name.toUpperCase();
   ans.email = ans.email.toUpperCase();
 
   res.send(ans);
-})
-
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
